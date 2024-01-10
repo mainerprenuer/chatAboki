e705ecd919a5c2329df59294cd4e3edd1a7d32c2
Mousty Mainer <moustymainer@gmail.com>
2024-01-10 22:34:08 +0100

    setup Clerk for Auth.. works on homepage.. signin/signout all up

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,13 @@
-import type { Config } from "tailwindcss"
-
-const config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   darkMode: ["class"],
   content: [
     './pages/**/*.{ts,tsx}',
     './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
     './src/**/*.{ts,tsx}',
-	],
-  prefix: "tailwind.config.ts",
+  ],
+  prefix: "",
   theme: {
     container: {
       center: true,
@@ -75,6 +74,4 @@ const config = {
     },
   },
   plugins: [require("tailwindcss-animate")],
-} satisfies Config
-
-export default config
+}
