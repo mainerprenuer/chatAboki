000168d6ed51e0b583f7272d5df77906fdb6d36c
Mousty Mainer <moustymainer@gmail.com>
2024-01-11 16:38:56 +0100

    setup file upload using dropzone..

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import FileUpload from "@/components/FileUpload";
 import { Button } from "@/components/ui/button";
 import { UserButton, auth } from "@clerk/nextjs";
 import { LogIn, LogInIcon } from "lucide-react";
@@ -25,7 +26,9 @@ export default async function Home() {
           </p>
 
           <div className="w-full mt-4">
-            {isAuth ? (<h1>fileUpload</h1>):(
+            {isAuth ? (
+              <FileUpload />
+            ):(
               <Link href="/sign-in">
                 <Button>Login to get Started!
                   <LogIn className="w-4 h-4 ml-2" />
