5a76b55132f0c72c208d4ef3e483f6636b5f82e0
Mousty Mainer <moustymainer@gmail.com>
2024-01-11 15:44:22 +0100

    setup neonDB, drizzle-kit and migrated the schema to pgDB

---
+++ drizzle.config.ts
@@ -0,0 +1,14 @@
+import {Config} from 'drizzle-kit';
+import * as dotenv from 'dotenv';
+dotenv.config({path: ".env"});
+
+export default {
+    driver: 'pg',
+    schema: './src/lib/db/schema.ts',
+    dbCredentials: {
+        connectionString: process.env.DATABASE_URL!,
+    }
+} satisfies Config;
+
+//  this ensures our neon db schema is synced to migration
+//npx drizzle-kit push:pg
