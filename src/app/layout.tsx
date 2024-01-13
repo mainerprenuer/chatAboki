c913f28b3bbfac7185b3fa7e6683437ca0e9696b
Mousty Mainer <moustymainer@gmail.com>
2024-01-13 11:42:34 +0100

    fileUpload comp setup with AWS s3 POST request success, added toaster and Axios for handling Queries!

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import { ClerkProvider } from '@clerk/nextjs'
 import Providers from '@/components/Providers'
+import {Toaster} from 'react-hot-toast'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,9 +19,10 @@ export default function RootLayout({
   return (
     <ClerkProvider>
      <Providers>
-     <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+        <Toaster />
+      </html>
      </Providers>
     </ClerkProvider>
   )
--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,23 +1,17 @@
-'use client'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import React from 'react'
+"use client";
+import React from "react";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 
 type Props = {
-    children: React.ReactNode;
+  children: React.ReactNode;
 };
 
 const queryClient = new QueryClient();
 
 const Providers = ({ children }: Props) => {
-    return (
-        <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-    )
-}
-
-const Provider = (props: Props) => {
   return (
-    <div>Provider</div>
-  )
-}
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+};
 
-export default Provider
+export default Providers;
