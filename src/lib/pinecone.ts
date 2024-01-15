3fbf80bf2f49b1eea938aef891a8c348b8749df2
Mousty Mainer <moustymainer@gmail.com>
2024-01-15 07:16:28 +0100

    pinecone bug fixed.. now able to download from S3

--- src/lib/pinecone.ts
+++ src/lib/pinecone.ts
@@ -1,6 +1,7 @@
 import { Pinecone } from '@pinecone-database/pinecone';
 import { downloadFromS3 } from './s3-server';
-import {PDFLoader} from 'langchain/document_loaders/fs/pdf'
+import { PDFLoader } from 'langchain/document_loaders/fs/pdf'
+import { Document, RecursiveCharacterTextSplitter } from '@pinecone-database/doc-splitter'
 
 let pinecone: Pinecone | null = null;
 
@@ -12,6 +13,13 @@ export const getPinecone = async () => {
         })
     }
     return pinecone
+};
+
+type PDFPage = {
+    pageContent: string;
+    metadata: {
+        loc: {pageNumber: number}
+    }
 }
 
 export async function loadS3IntoPinecone(fileKey: string) {
@@ -22,7 +30,32 @@ export async function loadS3IntoPinecone(fileKey: string) {
         throw new Error('could not download from s3')
     }
     const loader = new PDFLoader(file_name);
-    const pages = await loader.load();
-    return pages;
-      
+    const pages = (await loader.load()) as PDFPage[];
+
+    //2. split and segement the pdf into smaller paragraphs
+    // return pages;   
+    const documents = await Promise.all(pages.map(prepareDocument));
 }
+
+//truncate paragraphs to pinecone byte sizes 36000
+export const truncateStringByBytes = (str: string, bytes: number)=> {
+    const enc = new TextEncoder()
+    return new TextDecoder('utf-8').decode(enc.encode(str).slice(0,bytes))
+}
+
+async function prepareDocument(page: PDFPage) {
+        let {pageContent, metadata} = page
+        pageContent = pageContent.replace(/\n/g, '') //regex
+        //split the docs
+        const splitter = new RecursiveCharacterTextSplitter()
+        const docs = await splitter.splitDocuments([
+            new Document({
+                pageContent,
+                metadata: {
+                    pageNumber: metadata.loc.pageNumber,
+                    text: truncateStringByBytes(pageContent, 36000)
+                }
+            })
+        ])
+        return docs
+    }
